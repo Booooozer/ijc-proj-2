81d87497c828880cbfc9bec58556fac0654f1d78
Booooozer <xmatus37@vutbr.cz>
2021-04-08 00:56:56 +0200

    Completed tail implementation

--- tail.c
+++ tail.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_LINE 85
+#define MAX_LINE 512
 
 /* parser for -n NUM parameter
  * returns number of lines to be printed / from what line to print
@@ -11,7 +11,7 @@
  * returns true in a pointer to bool when '+' is specified
  */
 long int parseN(char *argv[], bool *plus) {
-    long int lines = 0;
+    long int lines = 10;
     char *left;
     if (strcmp(argv[1], "-n") == 0) {
         if (strstr(argv[2], "+") != NULL) {
@@ -22,8 +22,7 @@ long int parseN(char *argv[], bool *plus) {
             *plus = false;
         }
         lines = strtol(argv[2], &left, 10);
-        printf("%s\n", left);
-        if (strcmp(left, "") != 0) {
+        if (strcmp(left, "") != 0 || lines == 0) {
             lines = -1;
         }
     } else {
@@ -40,15 +39,37 @@ void removeLeftoverLine(FILE *fp) {
     } while (strstr(lineThrow, "\n") == NULL);
 }
 
+
+void printTail(FILE *fp, long int lines) {
+    char lineArr[lines][MAX_LINE];
+    bool printErr = true;
+    unsigned long count = 0;
+
+    while (fgets(lineArr[count % lines], MAX_LINE, fp) != NULL){
+        if (strstr(lineArr[count % lines], "\n") == NULL) {
+            //lineArr[count % lines][MAX_LINE - 2] = '\n';
+            removeLeftoverLine(fp);
+            if (printErr == true) {
+                printErr = false;
+                fprintf(stderr,"Line overflow detected\n");
+            }
+        }
+        count++;
+    }
+
+    for (int i = 0; i < lines; i++) {
+        if (strstr(lineArr[count % lines], "\n") == NULL) {
+            printf("%s\n", lineArr[(count + i) % lines]);
+        } else {
+            printf("%s", lineArr[(count + i) % lines]);
+        }
+    }
+}
+
 void printPlusTail(FILE *fp, long int lines) {
-    /*
-     * allocate lines*511 array
-     * cycle through it with fgets and print last 'lines' at the end
-     * somehow check for lines that are longer than 512
-     */
     bool printErr = true;
     char line[MAX_LINE];
-    unsigned long count = 0;
+    long int count = 0;
     while(fgets(line, MAX_LINE, fp) != NULL) {
         count++;
         if (count < lines)
@@ -68,15 +89,9 @@ void printPlusTail(FILE *fp, long int lines) {
 }
 
 int main(int argc, char* argv[]) {
-
-    if (argc > 4) {
-        fprintf(stderr, "Too many arguments\n");
-        return 1;
-    }
-
     long int lines = 10;
     bool plus = false;
-    char *left = NULL;
+    //char *left = NULL;
     FILE *fp = stdin;
 
     switch (argc) {
@@ -121,6 +136,8 @@ int main(int argc, char* argv[]) {
 
     if (plus == true) {
         printPlusTail(fp, lines);
+    } else if (plus == false) {
+        printTail(fp, lines);
     }
 
 
